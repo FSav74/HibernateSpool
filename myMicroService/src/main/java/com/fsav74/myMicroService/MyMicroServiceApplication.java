e811ef18cb6ed3afed419f6e3000053ef30ea948
fsav <fsletterese@yahoo.it>
2019-05-28 00:24:44 +0200

    commit iniziale

---
+++ myMicroService/src/main/java/com/fsav74/myMicroService/MyMicroServiceApplication.java
@@ -0,0 +1,13 @@
+package com.fsav74.myMicroService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyMicroServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyMicroServiceApplication.class, args);
+	}
+
+}
